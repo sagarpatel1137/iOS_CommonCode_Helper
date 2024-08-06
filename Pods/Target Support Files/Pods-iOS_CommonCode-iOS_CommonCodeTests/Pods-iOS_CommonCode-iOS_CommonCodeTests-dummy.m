3ec7f9f609825ced405e6be6392d27451b7ec188
Gautam iOS <>
2024-08-06 14:04:41 +0530

    Initial project setup

---
+++ Pods/Target Support Files/Pods-iOS_CommonCode-iOS_CommonCodeTests/Pods-iOS_CommonCode-iOS_CommonCodeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOS_CommonCode_iOS_CommonCodeTests : NSObject
+@end
+@implementation PodsDummy_Pods_iOS_CommonCode_iOS_CommonCodeTests
+@end
---
+++ iOS_CommonCode/iOS_CommonCode.h
@@ -0,0 +1,18 @@
+//
+//  iOS_CommonCode.h
+//  iOS_CommonCode
+//
+//  Created by IOS on 06/08/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iOS_CommonCode.
+FOUNDATION_EXPORT double iOS_CommonCodeVersionNumber;
+
+//! Project version string for iOS_CommonCode.
+FOUNDATION_EXPORT const unsigned char iOS_CommonCodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOS_CommonCode/PublicHeader.h>
+
+
